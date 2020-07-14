4ce3eabac974e4c58eded5b42eca2c8142645a95
Davi Inácio <aazz6850@gmail.com>
2020-07-13 23:55:23 -0300

    control with W, A, S, W

--- src/keyboard-listener.js
+++ src/keyboard-listener.js
@@ -2,12 +2,22 @@ export default function KeyboardListener(input, canvas){
     canvas.addEventListener('keydown', function(e){
         const keyName = e.code;
         const knownKeys = {
+            // Direction
             'ArrowUp': input.up,
             'ArrowDown': input.down,
             'ArrowLeft': input.left,
             'ArrowRight': input.right,
+
+            'KeyW': input.up,
+            'KeyS': input.down,
+            'KeyA': input.left,
+            'KeyD': input.right,
+
+            // Controls
             'KeyP': input.togglePlayPause,
             'Space': input.togglePlayPause,
+
+            // Special keys
             'KeyQ': input.toggleAssets
         };
         
